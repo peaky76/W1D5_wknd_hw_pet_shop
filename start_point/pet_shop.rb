817a9f22fbb890f26759c9078b0d25893900c771
peaky76 <robertjamespeacock@gmail.com>
2020-06-19 12:43:37 +0100

    test customer can afford pet pass

--- start_point/pet_shop.rb
+++ start_point/pet_shop.rb
@@ -68,3 +68,11 @@ end
 def add_pet_to_customer(customer, new_pet)
     customer[:pets] << new_pet
 end
+
+def customer_can_afford_pet(customer, pet)
+    if (customer[:cash] >= pet[:price])
+        return true
+    else
+        return false
+    end
+end
